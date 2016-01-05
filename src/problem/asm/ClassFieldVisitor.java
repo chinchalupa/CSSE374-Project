c6f951cbf9d79658a0a975d9a6d6544e819bcfef
Chinchalupa <wrightjt@rose-hulman.edu>
2016-01-04 23:03:51 -0500

    Includes some cleaner parsing for file names and inheritance.

--- src/problem/asm/ClassFieldVisitor.java
+++ src/problem/asm/ClassFieldVisitor.java
@@ -3,6 +3,18 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.Type;
 public class ClassFieldVisitor extends ClassVisitor{
+
+    private String type;
+    private String name;
+
+    public String getFieldType() {
+        return type;
+    }
+
+    public String getFieldName() {
+        return name;
+    }
+
     public ClassFieldVisitor(int api){
         super(api);
     }
@@ -13,6 +25,8 @@ public class ClassFieldVisitor extends ClassVisitor{
         FieldVisitor toDecorate = super.visitField(access, name, desc, signature, value);
         String type = Type.getType(desc).getClassName();
 //        System.out.println(" "+type+" "+ name);
+        this.type = type;
+        this.name = name;
 // TODO: add this field to your internal representation of the current class.
 // What is a good way to know what the current class is?
         return toDecorate;
