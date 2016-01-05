514ad25190626c911bfd3241dd5f0c80d6552cc9
Chinchalupa <wrightjt@rose-hulman.edu>
2016-01-04 23:08:03 -0500

    Miniscule change

--- src/problem/asm/ClassFieldVisitor.java
+++ src/problem/asm/ClassFieldVisitor.java
@@ -24,7 +24,7 @@ public class ClassFieldVisitor extends ClassVisitor{
     public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
         FieldVisitor toDecorate = super.visitField(access, name, desc, signature, value);
         String type = Type.getType(desc).getClassName();
-//        System.out.println(" "+type+" "+ name);
+        System.out.println(" "+type+" "+ name);
         this.type = type;
         this.name = name;
 // TODO: add this field to your internal representation of the current class.
